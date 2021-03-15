a4a79251cbd883720a3df53cb8895e6d98c3c3ee
Mr.Gordon <58670552+kylekgordon@users.noreply.github.com>
2021-03-15 03:06:54 -0500

    Update main.cpp

--- Assignments/P02/main.cpp
+++ Assignments/P02/main.cpp
@@ -1,43 +1,67 @@
-/**
- * This program uses a "getch" function which means "get character".
- * However, getch does not print the character to the terminal, it 
- * lets you decide what to do based on what character you are pressing.
- * 
- * You can test which characters are being pressed using thier ascii values. 
- * 
- * An ascii table here should be helpful 
- *      http://www.asciitable.com/
- * 
- * Some integer values for some keys:
- *      LowerCase Letters   = 97(a) -> 122(z)
- *      UpperCase Letters   = 65(A) -> 90(Z)
- *      Enter Key           = 10
- *      Space Bar           = 32
- *      
- *      Arrow Keys          = Have same values as some letters
- *                            so we can't distinguish between 
- *                            the two (in this context).
- * 
- * Code below is a basic example of using a "getch" function along with
- * searching an array of words for partial matches. 
- * 
- * https://repl.it/@rugbyprof/getchexample#main.cpp
- */
+///////////////////////////////////////////////////////////////////////////////
+// 
+// Author:           Kyle Gordon
+// Email:            kylekgordon@gmail.com
+// Label:            P02
+// Title:            Processing in Linear Time
+// Course:           3013-Algorithms
+// Semester:         Spring 2021
+//
+// Description:
+// 	This program uses a "getch" function which means "get character".
+// 	However, getch does not print the character to the terminal, it 
+//	lets you decide what to do based on what character you are pressing.
+// 
+//Some integer values for some keys:
+//     LowerCase Letters   = 97(a) -> 122(z)
+//     UpperCase Letters   = 65(A) -> 90(Z)
+//     Enter Key           = 10
+//     Space Bar           = 32
+//      
+//      Arrow Keys          = Have same values as some letters
+//                            so we can't distinguish between 
+//                            the two (in this context).
+// Usage:
+//      Run the program and it reads the dictionary from the file dict_w_defs.json.
+//
+// Files:     
+//      main.cpp    : driver program
+/////////////////////////////////////////////////////////////////////////////////
 
-#include "getch.hpp"
-#include "termcolor.hpp"
-#include "JsonFacade.hpp"
-#include "timer.hpp"
-#include <fstream>
-#include <iostream>
-#include <vector>
 
-using namespace std;
+
+struct Words
+{
+    string Word;            //word in dictionary
+    string Definition;      //definition of word
+    // default constructor
+    Words() 
+    {
+        Word = " ";
+        Definition = " ";
+    }
+    // overloaded constructor
+    Words(string w, string d)
+    {
+        Word = w;
+        Definition = d;
+    }
+};
+
+
+struct wordNode {
+    Words Data;    // Word struct
+    wordNode* Next;     // Link to next node
+    // constructor
+    wordNode(Words S) {
+        Data = S;
+        Next = NULL;
+    }
+};
 
 /**
  * Description:
- *      Finds partial matches in an array of strings and returns them. It
- *      doesn't matter where in the string the match is.
+ *      Finds partial matches in an array of strings and returns them.
  * Params:
  *      vector<string>  array       - array to search
  *      string          substring   - substring to search for in each word
@@ -53,37 +77,54 @@ vector<string> findWords(vector<string> array, string substring) {
     for (int i = 0; i < array.size(); i++) { // loop through array
         found = array[i].find(substring);    // check for substr match
         if (found != string::npos) {         // if found >= 0 (its found then)
-             if(int(found) == 0){				//if word found begins with substring
+        if(int(found) == 0){				//if word found begins with substring
 		matches.push_back(array[i]);     // add to matches
 		}
-        }
+            
+    	}
     }
-
     return matches;
 }
 
 int main() {
     char k;                 // holder for character being typed
     string word = "";       // var to concatenate letters to
-    vector<string> dictionary; // array of animal names
-    vector<string> matches; // any matches found in vector of animals
 
+    vector<string> dictionaryW; // array of words names
+    vector<string> matches; // any matches found in vector of animals
+    
     ofstream fout("temp.txt");
 
     Timer T;   // create a timer
     T.Start(); // start it
-	JsonFacade J("dict_w_defs.json");   // create instance of json class
-	vector<string> keys = J.getKeys();
-    dictionary = keys;
-	//animals = LoadAnimals("animal_names.txt");
+	
+     // create instance of json class
+    JsonFacade J("dict_w_defs.json");
+    Words* WArray = new Words[J.getSize()];
+    //cout << J.getSize();
+    int i = 0;                  // init counter
+    while (i < J.getSize()) {          // while more stuff to read
+        
+        WArray[i].Word = J.getKey(i);   // read the Word into the Words
+        dictionaryW.push_back(WArray[i].Word);                        //    at array location i
+        WArray[i].Definition = J.getValue(J.getKey(i));     // read the Definition into the Words
+                                //    at array location i
+        ++i;                    // increment our counter
+    }
 
     T.End(); // end the current timer
 
-    // print out how long it took to load the animals file
-    cout << T.Seconds() << " seconds to read in and print json" << endl;
-    cout << T.MilliSeconds() << " milli to read in and print json" << endl;
+    //Test Array
+    //cout << WArray[10].Word << endl;
+    //cout << WArray[10].Definition << endl;
 
-    cout << "Type keys and watch what happens. Type capital Z to quit." << endl;
+    // print out how long it took to load the dictionary file
+    cout << T.Seconds() << " seconds to read in and print json" << endl;
+    cout << T.MilliSeconds() << " milli to read in and print json" << endl
+        << endl;
+
+    cout << "Type keys to seach for words. Type capital Z to quit." << endl
+        << endl;
 
     // While capital Z is not typed keep looping
     while ((k = getch()) != 'Z') {
@@ -113,16 +154,19 @@ int main() {
         // Find any animals in the array that partially match
         // our substr word
         T.Start(); // start it
-        matches = findWords(dictionary, word);
+        matches = findWords(dictionaryW, word);
         T.End(); // end the current timer
-        cout << T.Seconds() << " seconds to read in and print json" << endl;
-        cout << T.MilliSeconds() << " milli to read in and print json" << endl;
+        //cout << T.Seconds() << " seconds to read in and print json" << endl;
+        //cout << T.MilliSeconds() << " milli to read in and print json" << endl;
 
         if ((int)k != 32) { // if k is not a space print it
             cout << "Keypressed: " << termcolor::on_yellow << termcolor::blue << k << " = " << (int)k << termcolor::reset << endl;
             cout << "Current Substr: " << termcolor::red << word << termcolor::reset << endl;
+            cout << fixed << matches.size() << " words found in " << setprecision(3) << termcolor::red << T.Seconds() << 
+		    termcolor::reset << " seconds" << endl;
             cout << "Words Found: ";
             cout << termcolor::green;
+
             // This prints out all found matches
             for (int i = 0; i < 10; i++) {
                 cout << matches[i] << " ";
@@ -130,8 +174,6 @@ int main() {
             cout << termcolor::reset << endl
                  << endl;
         }
-
     }
-
     return 0;
 }
